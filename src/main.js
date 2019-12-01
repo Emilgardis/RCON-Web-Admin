44a46e3c0fd3cfd815cfac0ccbbfa92d798dcac8
Antonin Verdier <antonin17@live.fr>
2019-12-01 12:26:32 +0100

    Added some comments to clarify how updating works

--- src/main.js
+++ src/main.js
@@ -50,6 +50,7 @@ if (mode == "update-core") {
     var request = require(__dirname + "/request");
     var fs = require("fs");
     var unzip = require("unzip");
+    //Get the update zip from the git repository
     request.get("https://codeload.github.com/lacaulac/rcon-web-admin/zip/master", true, function (contents) {
         if (!contents.length) {
             console.error("Cannot load rcon-web-admin zip file");
@@ -57,6 +58,7 @@ if (mode == "update-core") {
             return;
         }
         var dir = __dirname + "/..";
+        //Write the downloaded zip to a file
         fs.writeFile(dir + "/master.zip", contents, {"mode": 0o777}, function () {
             fs.createReadStream(dir + "/master.zip").pipe(unzip.Parse()).on('entry', function (entry) {
                 var fileName = entry.path.split("/").slice(1).join("/");
